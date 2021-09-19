d66e3487abb204618e491e20cef57e56c8e18aca
ruizqa <89569188+ruizqa@users.noreply.github.com>
2021-09-19 02:06:26 -0600

    initial commit

--- server.py
+++ server.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
+
 app = Flask(__name__)  
+fruits = ['strawberry', 'raspberry', 'blackberry', 'apple']
+total=0
 
 @app.route('/')         
 def index():
     return render_template("index.html")
 
 @app.route('/checkout', methods=['POST'])         
-def checkout():
+def checkout(fruits=fruits,total=total):
+    
+    dt_string = datetime.now().strftime("%B %d %Y %H:%M:%S")
+    c_fruits=[]
+
+    for fruit in fruits:
+        
+        if int(form_dict[fruit]) > 0:
+            total += int(form_dict[fruit])
+            c_fruits.append(fruit)
+            
     print(request.form)
-    return render_template("checkout.html")
+    print(f"Charging {form_dict['first_name']} {form_dict['last_name']} for {total} fruits")
+    return render_template("checkout.html" ,fruits=c_fruits, total=total, date = dt_string)
 
 @app.route('/fruits')         
 def fruits():
